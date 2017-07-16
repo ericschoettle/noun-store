21b73f23e61800e293aac2fc927496c46b0763d7
ericschoettle <eric.schoettle@gmail.com>
2017-07-15 19:42:48 -0700

    debug and add authentication on review form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :authorize, only: [:create]
+
+  def create
+    @item = Item.find(params[:item_id])
+    @review = @item.reviews.create(review_params)
+    @item.save
+
+    redirect_to item_path(@item)
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:body, :user_id)
+  end
 end
